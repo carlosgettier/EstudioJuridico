f9b61983b9d65bf74281e1f2e3b56a6d9fce1da0
Jose Manuel Sobrero Pratto <jmsobrero@icebergsolutions.com>
2024-04-22 11:18:55 -0300

    Cambio de comillas en errormodal.jsx

--- src/components/contact-modal/ErrorModal.jsx
+++ src/components/contact-modal/ErrorModal.jsx
@@ -5,7 +5,7 @@ const errorModal = (props) => (
     <div className="error__modal">
       <p>Oooops!!!</p>
       <p>
-        Something went wrong :{"("}</p>
+        Something went wrong :{'('}</p>
       <div role="button" tabIndex={0} className="modal__btn flex-center" onClick={props.closeModal} onKeyDown={props.closeModal}>
         <p>Ok</p>
       </div>
